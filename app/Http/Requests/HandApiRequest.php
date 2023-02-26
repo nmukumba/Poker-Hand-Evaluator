ec585408e8a050d08ebe46f74a5748a4567e409b
Norest Mukumba <norestmukumba@gmail.com>
2023-02-26 22:50:41 +0200

    add separate Hand request to handle api validation errors

---
+++ app/Http/Requests/HandApiRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class HandApiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'cards' => 'required|string'
+        ];
+    }
+
+    /**
+     * @param Validator $validator
+     * @return void
+     * @throws ValidationException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        $response = response()->json([
+            'success' => false,
+            'message' => array_values($validator->errors()->toArray())[0][0],
+        ], 400);
+
+        throw (new ValidationException($validator, $response))
+            ->errorBag($this->errorBag);
+    }
+}
